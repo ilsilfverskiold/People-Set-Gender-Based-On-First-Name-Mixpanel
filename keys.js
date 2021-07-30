1f786b732ec69d479c128a1463b99a9723d2838d
ida silfverskiold <ilsilfverskiold@gmail.com>
2021-07-30 15:40:08 +0200

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,7 @@
+
+// Exchange for your own keys 
+module.exports = {
+    mixpanel_api_key: "xxxxxxxxxxxxxxxxx",
+    mixpanel_token: "xxxxxxxxxxxxxxxxx",
+	genderize_api_key: "xxxxxxxxxxxxxxxxx"
+};
